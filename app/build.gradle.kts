b6600003e40f8be54f000c39bf6df091d3299b47
semyonsw <semyonsw@gmail.com>
2026-08-07 13:33:05 +0400

    made besamt karevor changes after testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "am.onex.stopdoom"
         minSdk = 34
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
 
     signingConfigs {
